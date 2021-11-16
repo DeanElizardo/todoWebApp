a6209d9474f527b36400c27bb82dcb7806c821f4
Dean Elizardo <del@delizardo.com>
2021-11-15 18:50:16 -0600

    wrote sortToDoList()

--- lists.js
+++ lists.js
@@ -7,9 +7,23 @@ const { body, validationResult } = require('express-validator');
 const morgan = require('morgan');
 const store = require('connect-loki');
 
-const app = express();
-
 let todoLists = require('./lib/seed-data');
+const sortToDoList = list => {
+  return list.slice().sort((todoListA, todoListB) => {
+    let titleA = todoListA.title;
+    let titleB = todoListB.title;
+
+    if (titleA < titleB) { //put titleA first
+      return -1;
+    } else if (titleA > titleB) { //put titleB first
+      return 1;
+    } else {
+      return 0; //retain original order
+    }
+  });
+}
+
+const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', './views');
@@ -18,8 +32,10 @@ app.use(morgan("dev")); //change "dev" -> "common" after completing development
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('lists');
-})
+  res.render('lists', { 
+    todoLists: sortToDoList(todoLists),
+  });
+});
 
 app.listen(PORT, HOST, () => {
   console.log(`Server listening on port ${PORT}`);
