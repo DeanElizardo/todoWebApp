363181005b927bfabf05c76e19e733e2c0e50fe3
Dean Elizardo <del@delizardo.com>
2021-11-28 15:29:10 -0600

    added support for data store and type persistence

--- lists.js
+++ lists.js
@@ -1,5 +1,8 @@
+//================================================================CONFIGURATION
 const HOST = '127.0.0.1';
 const PORT = 3000;
+
+//======================================================================MODULES
 const express = require('express');
 const session = require('express-session');
 const flash = require('express-flash');
@@ -9,7 +12,9 @@ const store = require('connect-loki');
 
 const ToDoList = require('./lib/todolist');
 const ToDo = require('./lib/todo');
-let todoLists = require('./lib/seed-data');
+//let todoLists = require('./lib/seed-data'); //testing seed data
+
+//=============================================================HELPER FUNCTIONS
 const sortList = list => {
   return list.slice().sort((todoListA, todoListB) => {
     let titleA = todoListA.title.toLowerCase();
@@ -32,21 +37,19 @@ const sortToDoList = list => {
   return sortList(notDone).concat(sortList(done));
 }
 
-function getTargetList(listID) {
+function getTargetList(todoLists, listID) {
   return todoLists.find(list => list.id.toString() === listID);
 }
 
-function getTargetItem(todoID) {
-  return getTargetList(todoID);
-}
-
+//====================================================================APP LOGIC
 const app = express();
 const LokiStore = store(session);
 
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-app.use(morgan("dev")); //change "dev" -> "common" after completing development
+//============================================================GLOBAL MIDDLEWARE
+app.use(morgan("dev"));
 app.use(session({
   cookie: {
     httpOnly: true,
@@ -54,20 +57,33 @@ app.use(session({
     path: "/",
     secure: false
   },
-  name: "Launch-school-toDo-web-app",
+  name: "Launch-school-todos-session-id",
   resave: false,
-  saveUninitialized:true,
-  secret: "Here's an insecure secret that should have been generated elsewhere and interpolated",
+  saveUninitialized: true,
+  secret: "Here's an insecure secret",
   store: new LokiStore({}),
 }));
 app.use(express.static('public'));
-app.use(express.urlencoded( { extended: false }));
+app.use(express.urlencoded({ extended: false }));
 app.use(flash());
+app.use((req, res, next) => {
+  let todoLists = [];
+  if ("todoLists" in req.session) {
+    req.session.todoLists.forEach(todoList => {
+      todoLists.push(ToDoList.makeTodoList(todoList));
+    });
+  }
+
+  req.session.todoLists = todoLists;
+
+  next();
+});
 app.use((req, res, next) => {
   res.locals.flash = req.session.flash;
   delete req.session.flash;
   next();
 });
+//=======================================================================ROUTES
 //==========================================================================GET
 app.get('/', (req, res) => {
   res.redirect('/lists');
@@ -75,7 +91,7 @@ app.get('/', (req, res) => {
 
 app.get('/lists', (req, res) => {
   res.render('lists', {
-    todoLists: sortToDoList(todoLists),
+    todoLists: sortToDoList(req.session.todoLists),
   });
 });
 
@@ -84,7 +100,7 @@ app.get('/lists/new', (req, res) => {
 });
 
 app.get('/lists/:id', (req, res) => {
-  let targetList = getTargetList(req.params.id);
+  let targetList = getTargetList(req.session.todoLists, req.params.id);
   res.render('manage-list', {
     todoList: targetList,
     todos: targetList.todos,
@@ -92,11 +108,12 @@ app.get('/lists/:id', (req, res) => {
 });
 
 app.get('/lists/:listID/edit', (req, res) => {
-  let targetList = getTargetList(req.params.listID);
+  let targetList = getTargetList(req.session.todoLists, req.params.listID);
   res.render('edit-list', {
     todoList: targetList,
   });
 });
+
 //=========================================================================POST
 app.post('/lists',
   [
@@ -108,13 +125,13 @@ app.post('/lists',
       .isLength({ max: 100 })
       .withMessage("Title must not be longer than 100 characters")
       .bail()
-      .custom(title => {
-        let characters = title.slice().toLowerCase();
-        let duplicate = todoLists
-              .find(list => list.title.toLowerCase() === characters);
+      .custom((title, { req }) => {
+        let duplicate = req.session.todoLists
+                .find(todo => todo.title.toLowerCase() === title.toLowerCase());
+
         return duplicate === undefined;
       })
-      .withMessage("List title must be unique.")
+      .withMessage("Title must be unique")
   ],
   (req, res, next) => {
     const errors = validationResult(req);
@@ -132,11 +149,11 @@ app.post('/lists',
         todoListTitle: title,
       });
     } else {
-      todoLists.push(new ToDoList(title));
+      req.session.todoLists.push(new ToDoList(title));
       req.flash("Success", "New list added");
       res.redirect("/lists");
     }
-});
+  });
 
 app.post('/lists/:id/todos',
   [
@@ -155,7 +172,7 @@ app.post('/lists/:id/todos',
     next();
   },
   (req, res) => {
-    let targetList = getTargetList(req.params.id);
+    let targetList = getTargetList(req.session.todoLists, req.params.id);
     if (req.session.flash) {
       res.render('manage-list', {
         todoList: targetList,
@@ -172,7 +189,7 @@ app.post('/lists/:id/todos',
 );
 
 app.post('/lists/:listID/todos/:todoID/toggle', (req, res) => {
-  let targetList = getTargetList(req.params.listID);
+  let targetList = getTargetList(req.session.todoLists, req.params.listID);
   let targetTodo = targetList.findById(Number(req.params.todoID));
 
   if (targetTodo.isDone()) {
@@ -185,7 +202,7 @@ app.post('/lists/:listID/todos/:todoID/toggle', (req, res) => {
 });
 
 app.post('/lists/:listID/complete_all', (req, res) => {
-  let targetList = getTargetList(req.params.listID);
+  let targetList = getTargetList(req.session.todoLists, req.params.listID);
 
   targetList.markAllDone();
 
@@ -201,14 +218,13 @@ app.post('/lists/:listID/edit',
       .bail()
       .isLength({ max: 100 })
       .withMessage("Title must not be longer than 100 characters")
-      .bail()
-      .custom(title => {
-        let characters = title.slice().toLowerCase();
-        let duplicate = todoLists
-              .find(list => list.title.toLowerCase() === characters);
+      .custom((title, { req }) => {
+        let duplicate = req.session.todoLists
+                .find(todo => todo.title.toLowerCase() === title.toLowerCase());
+                
         return duplicate === undefined;
       })
-      .withMessage("List title must be unique.")
+      .withMessage("Title must be unique")
   ],
   (req, res, next) => {
     const errors = validationResult(req);
@@ -219,14 +235,15 @@ app.post('/lists/:listID/edit',
     next();
   },
   (req, res) => {
-    let targetList = getTargetList(req.params.listID);
+    let targetList = getTargetList(req.session.todoLists, req.params.listID);
+    
     if (req.session.flash) {
       res.render('edit-list', {
         flash: req.session.flash,
         todoList: targetList
       });
     } else {
-      let targetList = getTargetList(req.params.listID);
+      let targetList = getTargetList(req.session.todoLists, req.params.listID);
       targetList.setTitle(req.body.todoListTitle);
       req.flash("Success", "List edited");
       res.redirect("/lists");
@@ -235,15 +252,16 @@ app.post('/lists/:listID/edit',
 );
 
 app.post('/lists/:listID/destroy', (req, res) => {
-  let targetList = getTargetList(req.params.listID);
-  let removalIdx = todoLists.indexOf(targetList);
-  todoLists.splice(removalIdx, 1);
+  let targetList = getTargetList(req.session.todoLists, req.params.listID);
+  let removalIdx = req.session.todoLists.indexOf(targetList);
+  
+  req.session.todoLists.splice(removalIdx, 1);
 
   res.redirect('/lists');
 });
 
 app.post('/lists/:listID/todos/:todoID/destroy', (req, res) => {
-  let targetList = getTargetList(req.params.listID);
+  let targetList = getTargetList(req.session.todoLists, req.params.listID);
   let targetTodo = targetList.findById(Number(req.params.todoID));
   let todoIndex = targetList.findIndexOf(targetTodo);
 
@@ -252,6 +270,7 @@ app.post('/lists/:listID/todos/:todoID/destroy', (req, res) => {
   res.redirect(`/lists/${req.params.listID}`);
 });
 
+//===================================================================RUN SERVER
 app.listen(PORT, HOST, () => {
   console.log(`Server listening on port ${PORT}`);
 });
